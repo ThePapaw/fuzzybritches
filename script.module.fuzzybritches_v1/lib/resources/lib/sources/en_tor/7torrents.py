6e7f2b33511d7e452b856f07831649ecfea4ca09
The Papaw <20951535+Wanderer189@users.noreply.github.com>
2020-11-12 06:13:25 -0600

    scraper work

--- script.module.fuzzybritches_v1/lib/resources/lib/sources/en_tor/7torrents.py
+++ script.module.fuzzybritches_v1/lib/resources/lib/sources/en_tor/7torrents.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
-'''
-**Created by Tempest**
+###############################################################################
+#                           "A BEER-WARE LICENSE"                             #
+# ----------------------------------------------------------------------------#
+# Feel free to do whatever you wish with this file. Since we most likey will  #
+# never meet, buy a stranger a beer. Give credit to ALL named, unnamed, past, #
+# present and future dev's of this & files like this. -Share the Knowledge!   #
+###############################################################################
 
+# Addon Name: Fuzzy Britches
+# Addon id: script.module.fuzzybritches
+# Addon Provider: The Papaw
+
+'''
+Included with the Fuzzy Britches Add-on
 '''
 
 import re, urllib, urlparse
